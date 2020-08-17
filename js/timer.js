b9c7270ee93b8b88f32a11148f56c32658d78538
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-08-17 23:27:10 +0300

    create anonymous self-execution function and assignment it to timer const

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,11 @@
 const buttons = document.querySelectorAll("[data-time]"),
   form = document.forms["customForm"],
   inputMinutes = form.elements["minutes"];
+
+const timer = (function () {
+  return {
+    init,
+    start,
+    stop,
+  };
+})();
